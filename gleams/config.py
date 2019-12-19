cebf09b9661f76f02a17fdec358276b3b7ed961d
Wout <wout@bittremieux.be>
2019-12-19 14:17:36 -0800

    Decrease Faiss batch size to avoid excessive cudaMalloc warnings

--- gleams/config.py
+++ gleams/config.py
@@ -78,8 +78,8 @@ max_num_pairs_val = 500000
 # ANN indexing.
 num_probe = 1024
 num_neighbors = 50
-ann_add_batch_size = 2**16
-ann_search_batch_size = 2**16
+ann_add_batch_size = 2**9
+ann_search_batch_size = 2**9
 
 # DBSCAN clustering.
 # TODO: Figure out good hyperparameters.
