3f20e09cb451c5bf639eb70f7d8903f9573629a4
Wout <wout@bittremieux.be>
2020-09-21 15:31:02 -0700

    Use a ramp loss for positive pairs

--- gleams/nn/embedder.py
+++ gleams/nn/embedder.py
@@ -75,9 +75,9 @@ def contrastive_loss(y_true, y_pred):
     -------
     The contrastive loss between the true and predicted class labels.
     """
-    square_pred = K.square(y_pred)
+    ramp_square = K.minimum(K.square(y_pred), config.margin)
     margin_square = K.square(K.maximum(config.margin - y_pred, 0))
-    return K.mean(y_true * config.loss_label_certainty * square_pred +
+    return K.mean(y_true * config.loss_label_certainty * ramp_square +
                   (1 - y_true * config.loss_label_certainty) * margin_square)
 
 
