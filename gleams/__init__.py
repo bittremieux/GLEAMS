fb4d5616c75ef44a98802f77ea61a2af3d4cdd7b
Wout <wout@bittremieux.be>
2021-10-04 11:33:40 -0700

    Get version number from package

--- gleams/__init__.py
+++ gleams/__init__.py
@@ -1,1 +1,16 @@
-__version__ = '0.1.0'
+pkg_name = 'gleams'
+
+try:
+    from importlib.metadata import version, PackageNotFoundError
+
+    try:
+        __version__ = version(pkg_name)
+    except PackageNotFoundError:
+        __version__ = 'unspecified'
+except ImportError:
+    from pkg_resources import get_distribution, DistributionNotFound
+
+    try:
+        __version__ = get_distribution(pkg_name).version
+    except DistributionNotFound:
+        __version__ = 'unspecified'
